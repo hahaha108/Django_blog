32891c3ca83b31797b406ab4c0bdfc461dcd12ad
hahaha108 <369943318@qq.com>
2018-04-18 20:37:27 +0800

    添加说明

--- Django_blog/settings.py
+++ Django_blog/settings.py
@@ -147,9 +147,16 @@ WSGI_APPLICATION = 'Django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': '123456',
     }
 }
 
